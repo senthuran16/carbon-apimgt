80d79b926ab3c2505819573066c8bb0781cef9c3
Malintha Amarasinghe <malintha.prasan@gmail.com>
2020-01-02 10:18:23 +0530

    Reverted #1765 as it breaks 3rd Party Key Managers
    This reverts commit 23b40c34331cd79f18e12343e40a19119b4d50a0, reversing
    changes made to f1963295efd49c63474333c1054a45eea2e1a124.

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/DefaultKeyValidationHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/DefaultKeyValidationHandler.java
@@ -33,7 +33,6 @@ import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.model.AccessTokenDO;
-import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
 import org.wso2.carbon.identity.oauth2.validators.OAuth2ScopeValidator;
 import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
@@ -145,23 +144,13 @@ public class DefaultKeyValidationHandler extends AbstractKeyValidationHandler {
             }
         }
 
-        AuthenticatedUser user = null;
-        try {
-            user = OAuth2Util.getAccessTokenDOfromTokenIdentifier(validationContext.getAccessToken()).
-                    getAuthzUser();
-            if (user == null) {
-                user = new AuthenticatedUser();
-                user.setUserName(MultitenantUtils.getTenantAwareUsername(apiKeyValidationInfoDTO.getEndUserName()));
-                user.setTenantDomain(apiKeyValidationInfoDTO.getSubscriberTenantDomain());
-                if (user.getUserName() != null && APIConstants.FEDERATED_USER
-                        .equalsIgnoreCase(IdentityUtil.extractDomainFromName(user.getUserName()))) {
-                    user.setFederatedUser(true);
-                }
-            }
-        } catch (IdentityOAuth2Exception e) {
-            log.error("ERROR while retrieving user during token validation " + e.getMessage(), e);
-            apiKeyValidationInfoDTO.setAuthorized(false);
-            apiKeyValidationInfoDTO.setValidationStatus(APIConstants.KeyValidationStatus.API_AUTH_GENERAL_ERROR);
+        AuthenticatedUser user = new AuthenticatedUser();
+        user.setUserName(MultitenantUtils.getTenantAwareUsername(apiKeyValidationInfoDTO.getEndUserName()));
+        user.setTenantDomain(apiKeyValidationInfoDTO.getSubscriberTenantDomain());
+
+        if (user.getUserName() != null && APIConstants.FEDERATED_USER
+                .equalsIgnoreCase(IdentityUtil.extractDomainFromName(user.getUserName()))) {
+            user.setFederatedUser(true);
         }
 
         AccessTokenDO accessTokenDO = new AccessTokenDO(apiKeyValidationInfoDTO.getConsumerKey(), user, scopes, null,
